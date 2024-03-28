86a6413fd623fa99048d46d505a63f885c7b6f8b
LostMyPillow <47119111+lostmypillow@users.noreply.github.com>
2024-03-28 21:48:38 +0800

    fix apostrophe

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-4">
       <h1 className="flex text-3xl font-bold mt-4">Lost My Pillow</h1>
-      <h2 className="flex text-base text-gray-600 font-medium pt-2 mb-4">Johnny L's Personal Site</h2>
+      <h2 className="flex text-base text-gray-600 font-medium pt-2 mb-4">Johnny L&apos;s Personal Site</h2>
       <div className="flex flex-col grow items-center justify-start ">
         <a
           className="text-xl border-2 border-blue-300 rounded-full px-4 py-2 m-4 hover:bg-blue-300 hover:text-slate-50"

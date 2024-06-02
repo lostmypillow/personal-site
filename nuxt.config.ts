0ba223e0b52aee014c46d9ebbc679d83530b07da
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-06-02 21:34:09 +0800

    rebase to Vite again

--- nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  css: ['~/assets/main.css'],
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
-})
---
+++ src/components/TableRow.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(['data'])
+
+
+</script>
+
+<template>
+    <tr>
+        <th>{{ data.logo }}</th>
+        <td>{{ data.name }}</td>
+        <td>{{ data.description }}</td>
+        <td>
+            <a class="btn" :href=data.demo>Demo</a>
+        </td>
+        <td>
+            <a class="btn" :href=data.github>GitHub</a>
+        </td>
+
+    </tr>
+</template>
